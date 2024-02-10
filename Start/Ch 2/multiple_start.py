fc23b9bc3fe4e4f141843cc47ec27b76c3b99b7e
Mir-Aadil1 <ermiraadil@gamil.com>
2024-02-10 12:01:25 +0530

    added multiple_start.py

--- Start/Ch 2/multiple_start.py
+++ Start/Ch 2/multiple_start.py
@@ -6,17 +6,31 @@ class A:
     def __init__(self):
         super().__init__()
         self.prop1 = "prop1"
+        self.name = "Class A"
 
 
 class B:
     def __init__(self):
         super().__init__()
         self.prop2 = "prop2"
+        self.name = "Class B"
 
 
 class C(A, B):
     def __init__(self):
         super().__init__()
 
+    def showprops(self):
+        print(self.prop1)
+        print(self.prop2)
+        print(self.name)
 
 c = C()
+c.showprops()
+
+print(C.__mro__)
+
+#(<class '__main__.C'>, <class '__main__.A'>, <class '__main__.B'>, <class 'object'>)
+
+
+# Method lookup resolution order 
