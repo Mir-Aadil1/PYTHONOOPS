e90445689d770958991a846fa770d5c02c661a43
Mir-Aadil1 <ermiraadil@gamil.com>
2024-02-11 12:12:01 +0530

    solved ch2 challenge

--- Start/Ch 2/challenge.py
+++ Start/Ch 2/challenge.py
@@ -12,14 +12,36 @@
 # For stocks: "Ticker: Company -- $Price"
 # For bonds: "description: duration'yr' : $price : yieldamt%"
 
-class Asset():
-    pass
+from abc import ABC, abstractmethod
 
-class Stock():
-    pass
 
-class Bond():
-    pass
+class Asset(ABC):
+    def __init__(self, price):
+        self.price = price
+
+    @abstractmethod
+    def get_description(self):
+        pass 
+
+class Stock(Asset):
+    def __init__(self, ticker, price, company):
+        super().__init__(price)
+        self.company = company
+        self.ticker = ticker
+
+
+    def get_description(self):
+        return f"{self.ticker}: {self.company} -- ${self.price}"
+
+class Bond(Asset):
+    def __init__(self,price, description, duration, yieldamt):
+        super().__init__(price)
+        self.description = description
+        self.duration = duration
+        self.yieldamt = yieldamt
+
+    def get_description(self):
+        return f"{self.description}: {self.duration}yr : ${self.price}: {self.yieldamt}%"
 
 
 # ~~~~~~~~~ TEST CODE ~~~~~~~~~
