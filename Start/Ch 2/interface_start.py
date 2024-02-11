98abc0d0e0e74d6f49e4d3b38ffe1e6d45f731eb
Mir-Aadil1 <ermiraadil@gamil.com>
2024-02-11 11:44:45 +0530

    interface

--- Start/Ch 2/interface_start.py
+++ Start/Ch 2/interface_start.py
@@ -12,14 +12,22 @@ class GraphicShape(ABC):
     def calcArea(self):
         pass
 
+class JSONIfy(ABC):
+    @abstractmethod
+    def toJSON(self):
+        pass
 
-class Circle(GraphicShape):
+class Circle(GraphicShape, JSONIfy):
     def __init__(self, radius):
         self.radius = radius
 
     def calcArea(self):
         return 3.14 * (self.radius ** 2)
+    
+    def toJSON(self):
+        return f"{{'Circle': {str(self.calcArea())}}}"
 
 
 c = Circle(10)
 print(c.calcArea())
+print(c.toJSON())
